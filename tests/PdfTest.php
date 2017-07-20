ed988ce1764be03338d1a5dd58634e734a6cbb5b
Tony Blanco <tblanco82@gmail.com>
2017-07-20 00:50:41 -0300

    * New test testCountWordsInPlaintext

--- tests/PdfTest.php
+++ tests/PdfTest.php
@@ -35,4 +35,25 @@ class PageTest extends TestCase
         empty($text)
       );
     }
+
+    /**
+     * Test that it counts words from plain text
+     * i'll be expecting an array of integers where the
+     * keys are words being repeated
+     * @return [type] [description]
+     */
+    public function testCountWordsInPlaintext(){
+      $dummyFile = storage_path().'/data/flow.pdf';
+      $text = ReadPdf::getText($dummyFile);
+      $text = strtolower($text);
+      $list_reps = array_count_values(
+        str_word_count($text, 1)
+      );
+      $any_word = array_rand($list_reps);
+      $any_rep = array_rand( array_keys($list_reps) );
+
+      $this->assertInternalType('string', $any_word);
+      $this->assertInternalType('integer', $any_rep);
+
+    }
 }
