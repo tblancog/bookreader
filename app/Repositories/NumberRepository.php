5f953154731fd2a69daa94806897e15618b0a6f4
Tony Blanco <tblanco82@gmail.com>
2017-07-20 02:41:52 -0300

    * Fixed validation with prime numbers

--- app/Repositories/NumberRepository.php
+++ app/Repositories/NumberRepository.php
@@ -4,6 +4,8 @@ namespace App\Repositories;
 class NumberRepository{
 
   public static function isPrime($number){
+    if($number == 1) return false;
+
     for($x=2; $x < $number; $x++){
       if( $number % $x == 0){
   		   return false;
